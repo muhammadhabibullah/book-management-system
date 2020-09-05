62bbc3a2ed2ce669b0bc3e6402fcd6759b8dffc5
Muhammad Habibullah <muhammad.habibullah@ralali.com>
2020-09-05 16:16:48 +0700

    automigrate only in debug mode

--- repositories/mysql/mysql.go
+++ repositories/mysql/mysql.go
@@ -22,7 +22,8 @@ var (
 // Init returns mysqlDB connection instance
 func Init() *gorm.DB {
 	once.Do(func() {
-		dsn := getMySQLConnString()
+		cfg := configs.GetConfig()
+		dsn := getMySQLConnString(cfg.Mysql)
 
 		var err error
 		mysqlDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -30,31 +31,29 @@ func Init() *gorm.DB {
 			log.Fatalf("failed to connect to mysql database: %s", err)
 		}
 
-		if err = mysqlDB.Set("gorm:table_options", "ENGINE=InnoDB").
-			AutoMigrate(
-				&models.Book{},
-			); err != nil {
-			log.Fatalf("failed to migrate new model to mysql database: %s", err)
+		if cfg.Debug {
+			if err = mysqlDB.Set("gorm:table_options", "ENGINE=InnoDB").
+				AutoMigrate(
+					&models.Book{},
+				); err != nil {
+				log.Fatalf("failed to migrate new model to mysql database: %s", err)
+			}
 		}
 
-		configMySQLConn()
+		configMySQLConn(cfg.Mysql)
 	})
 
 	return mysqlDB
 }
 
 // getMySQLConnString return connection string from config
-func getMySQLConnString() string {
-	cfg := configs.GetConfig().Mysql
-
+func getMySQLConnString(cfg configs.MySQLConfig) string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		cfg.User, cfg.Pass, cfg.Host, cfg.Port, cfg.Name)
 }
 
 // configMySQLConn configure MySQLConnection settings
-func configMySQLConn() {
-	cfg := configs.GetConfig().Mysql
-
+func configMySQLConn(cfg configs.MySQLConfig) {
 	db, _ := mysqlDB.DB()
 	db.SetMaxIdleConns(cfg.MaxIdleConn)
 	db.SetMaxOpenConns(cfg.MaxOpenConn)
